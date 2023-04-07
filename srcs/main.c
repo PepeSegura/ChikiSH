232a55051efe98a590b31af6aa96af4cc65afdda
Pepe Segura <pepeseguracarreno@gmail.com>
2023-04-07 21:38:55 +0200

    Fri Apr  7 21:38:  prueba 4

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pepe <pepe@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:37:31 by psegura-          #+#    #+#             */
-/*   Updated: 2023/04/04 19:54:32 by pepe             ###   ########.fr       */
+/*   Updated: 2023/04/07 21:38:50 by pepe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_cosas	g_c;
 int	main(int argc, char **argv, char **env)
 {
 	(void)argc, (void)argv;
+
 	g_c.env = ft_get_env(env);
 	g_c.dolar_q = 0;
 	manage_signal();
