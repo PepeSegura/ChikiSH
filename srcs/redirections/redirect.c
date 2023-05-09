ec3cf37b5a45de4942e9a5147bd3505b84ce22aa
agserran <agustinsscc01@gmail.com>
2023-05-09 21:39:01 +0200

    redirecciones

--- srcs/redirections/redirect.c
+++ srcs/redirections/redirect.c
@@ -6,7 +6,7 @@
 /*   By: agserran <agserran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:23:43 by agserran          #+#    #+#             */
-/*   Updated: 2023/05/09 21:29:07 by agserran         ###   ########.fr       */
+/*   Updated: 2023/05/09 21:38:48 by agserran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,19 @@ void    open_redict(t_info_cmd *info)
      {
         if (info->re->type == 2 || info->re->type == 3)
         {
-            if (fd[1] != -1)
-                close(fd[1]);
             if (info->re->type == 2)
                 fd[1] = open(info->re->file, O_CREAT | O_TRUNC, 0644);
             if (info->re->type == 3)
                 fd[1] = open(info->re->file, O_CREAT | O_APPEND, 0644);
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[1]);
         }
             fd[1] = open(info->re->file, O_CREAT | O_APPEND, 0644);
         if (info->re->type == 1 ||info->re->type == 3)
         {
-            if (fd[0] != -1)
-                close(fd[0]);
             fd[0] = open(info->re->file, O_RDONLY);
+            dup2(fd[0], STDIN_FILENO);
+            close(fd[0]);
         }
         info->re = info->re->next;
      }
